6f5089f9c20ddebcdbff7092da0047bf0bed24be
kemal-faza <kemalfaza26@gmail.com>
2026-06-30 14:22:10 +0700

    fix: add option interpreter: none to ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -15,6 +15,7 @@ module.exports = {
 		{
 			name: 'aksana-fe',
 			cwd: './apps/frontend',
+			interpreter: 'none',
 			script: './node_modules/.bin/next',
 			args: 'start -p 3000',
 			env: { DOTENV_CONFIG_PATH: '.env' },
@@ -22,6 +23,7 @@ module.exports = {
 		{
 			name: 'aksana-fe-staging',
 			cwd: './apps/frontend',
+			interpreter: 'none',
 			script: './node_modules/.bin/next',
 			args: 'start -p 3001',
 			env: { DOTENV_CONFIG_PATH: '.env.staging' },
